068ddbe9b04e2cd893b914419a1f42fa8f7ecc6f
Agustin Ortu <ortu.agustin@gmail.com>
2017-10-25 15:39:07 -0300

    muevo todo a /private

--- private/autoloader.php
+++ private/autoloader.php
@@ -1,25 +1,25 @@
 <?php
-require_once "./vendor/Twig/lib/Twig/Autoloader.php";
+require_once "./private/vendor/Twig/lib/Twig/Autoloader.php";
 
-require_once "./controllers/Controller.php";
-require_once "./controllers/Router.php";
-require_once "./controllers/IndexController.php";
-require_once "./controllers/UserController.php";
-require_once "./controllers/LoginController.php";
+require_once "./private/controllers/Controller.php";
+require_once "./private/controllers/Router.php";
+require_once "./private/controllers/IndexController.php";
+require_once "./private/controllers/UserController.php";
+require_once "./private/controllers/LoginController.php";
 require_once "./private/Session.php";
 
-require_once "./models/PDORepository.php";
-require_once "./models/UserRepository.php";
-require_once "./models/User.php";
+require_once "./private/models/PDORepository.php";
+require_once "./private/models/UserRepository.php";
+require_once "./private/models/User.php";
 
-require_once "./views/TwigView.php";
-require_once "./views/IndexView.php";
-require_once "./views/LoginView.php";
-require_once "./views/InvalidArgsView.php";
-require_once "./views/ErrorMessageView.php";
+require_once "./private/views/TwigView.php";
+require_once "./private/views/IndexView.php";
+require_once "./private/views/LoginView.php";
+require_once "./private/views/InvalidArgsView.php";
+require_once "./private/views/ErrorMessageView.php";
 
-require_once "./views/UserListView.php";
-require_once "./views/UserAddedView.php";
-require_once "./views/UserFormView.php";
-require_once "./views/UserUpdatedView.php";
-require_once "./views/UserDestroyedView.php";
+require_once "./private/views/UserListView.php";
+require_once "./private/views/UserAddedView.php";
+require_once "./private/views/UserFormView.php";
+require_once "./private/views/UserUpdatedView.php";
+require_once "./private/views/UserDestroyedView.php";
--- private/views/TwigView.php
+++ private/views/TwigView.php
@@ -7,7 +7,7 @@ abstract class TwigView
   {
     if (!isset(self::$twig)) {
       Twig_Autoloader::register();
-      $loader = new Twig_Loader_Filesystem('./templates');
+      $loader = new Twig_Loader_Filesystem('./private/templates');
       self::$twig = new Twig_Environment($loader);
     }
     return self::$twig;
