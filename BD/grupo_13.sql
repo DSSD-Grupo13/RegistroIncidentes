65743a9a4f932401c1518fcd2a30c1040da55b63
Agustin Ortu <ortu.agustin@gmail.com>
2017-10-31 21:39:55 -0300

    Base de datos: - Saco todos los comentarios de mierda de phpmyadmin - El charset en todas las tablas siempre UTF8 - Corregido campo 'nombe' --> 'nombre' en tabla 'tipoincidente' - Saco los incidentes de prueba

--- BD/grupo_13.sql
+++ BD/grupo_13.sql
@@ -1,51 +1,16 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 28-10-2017 a las 16:15:04
--- Versión del servidor: 10.1.25-MariaDB
--- Versión de PHP: 5.6.31
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `grupo_13`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `estado`
---
-
 CREATE TABLE `estado` (
   `idEstado` int(2) NOT NULL,
   `nombre` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `estado`
---
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `estado` (`idEstado`, `nombre`) VALUES
 (1, 'pendiente'),
-(2, 'espPresupuesto');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `incidente`
---
+(2, 'en-presupuesto');
 
 CREATE TABLE `incidente` (
   `idIncidente` int(10) NOT NULL,
@@ -55,41 +20,17 @@ CREATE TABLE `incidente` (
   `fechaInicio` date NOT NULL,
   `fechaFin` date NOT NULL,
   `idEstado` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `incidente`
---
-
-INSERT INTO `incidente` (`idIncidente`, `descripcion`, `idTipoIncidente`, `idUsuario`, `fechaInicio`, `fechaFin`, `idEstado`) VALUES
-(3, 'Incendio casa', 1, 1, '2017-10-26', '0000-00-00', 1),
-(4, 'Choque auto', 2, 2, '2017-10-26', '0000-00-00', 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tipoincidente`
---
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `tipoincidente` (
-  `nombe` varchar(15) NOT NULL,
+  `nombre` varchar(15) NOT NULL,
   `idTipoIncidente` int(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Volcado de datos para la tabla `tipoincidente`
---
-
-INSERT INTO `tipoincidente` (`nombe`, `idTipoIncidente`) VALUES
+INSERT INTO `tipoincidente` (`nombre`, `idTipoIncidente`) VALUES
 ('casa', 1),
 ('vehiculo', 2),
-('objetoMueble', 3);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuario`
---
+('objeto-mueble', 3);
 
 CREATE TABLE `usuario` (
   `idUsuario` int(5) NOT NULL,
@@ -98,88 +39,44 @@ CREATE TABLE `usuario` (
   `nombre` varchar(20) NOT NULL,
   `apellido` varchar(20) NOT NULL,
   `dni` int(8) NOT NULL,
-  `mail` varchar(50) NOT NULL,
-  `localidad` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `mail` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Volcado de datos para la tabla `usuario`
---
+INSERT INTO `usuario` (`idUsuario`, `nombreUsuario`, `contrasena`, `nombre`, `apellido`, `dni`, `mail`) VALUES
+(1, 'mlopez', 'bpm', 'maria', 'lopez', 38951674, 'mlopez@gmail.com'),
+(2, 'mgomez', 'bpm', 'mateo', 'gomez', 37694301, 'mgomez@gmail.com'),
+(3, 'jrodriguez', 'bpm', 'juan', 'rodriguez', 36950167, 'jrodriguez@gmail.com'),
+(4, 'wgarcia', 'bpm', 'walter', 'garcia', 33694178, 'wgarcia64@gmail.com');
 
-INSERT INTO `usuario` (`idUsuario`, `nombreUsuario`, `contrasena`, `nombre`, `apellido`, `dni`, `mail`, `localidad`) VALUES
-(1, 'mlopez', 'bpm', 'maria', 'lopez', 38951674, 'mlopez@gmail.com', 'la plata'),
-(2, 'mgomez', 'bpm', 'mateo', 'gomez', 37694301, 'mgomez@gmail.com', 'la plata'),
-(3, 'jrodriguez', 'bpm', 'juan', 'rodriguez', 36950167, 'jrodriguez@gmail.com', 'la plata'),
-(4, 'wgarcia', 'bpm', 'walter', 'garcia', 33694178, 'wgarcia64@gmail.com', 'la plata');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `estado`
---
 ALTER TABLE `estado`
   ADD PRIMARY KEY (`idEstado`);
 
---
--- Indices de la tabla `incidente`
---
 ALTER TABLE `incidente`
   ADD PRIMARY KEY (`idIncidente`),
   ADD KEY `idTipoIncidente` (`idTipoIncidente`),
   ADD KEY `idUsuario` (`idUsuario`),
   ADD KEY `id_estado` (`idEstado`);
 
---
--- Indices de la tabla `tipoincidente`
---
 ALTER TABLE `tipoincidente`
   ADD PRIMARY KEY (`idTipoIncidente`);
 
---
--- Indices de la tabla `usuario`
---
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`idUsuario`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `estado`
---
 ALTER TABLE `estado`
   MODIFY `idEstado` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT de la tabla `incidente`
---
+
 ALTER TABLE `incidente`
   MODIFY `idIncidente` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT de la tabla `tipoincidente`
---
+
 ALTER TABLE `tipoincidente`
   MODIFY `idTipoIncidente` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT de la tabla `usuario`
---
+
 ALTER TABLE `usuario`
   MODIFY `idUsuario` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- Restricciones para tablas volcadas
---
 
---
--- Filtros para la tabla `incidente`
---
 ALTER TABLE `incidente`
   ADD CONSTRAINT `incidente_ibfk_1` FOREIGN KEY (`idTipoIncidente`) REFERENCES `tipoincidente` (`idTipoIncidente`),
   ADD CONSTRAINT `incidente_ibfk_2` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`),
   ADD CONSTRAINT `incidente_ibfk_3` FOREIGN KEY (`idEstado`) REFERENCES `estado` (`idEstado`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
