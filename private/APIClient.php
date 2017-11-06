861f9299e79880efed36cc5a4b4d1f4d1cbe391f
Ortu Agustin <ortu.agustin@gmail.com>
2017-11-06 14:37:09 -0300

    clase APIClient para enviar los requerimientos HTTP

---
+++ private/APIClient.php
@@ -0,0 +1,50 @@
+<?php
+use GuzzleHttp\Exception\RequestException;
+
+class BadRequestException extends Exception
+{
+  public function __construct($json_error_response)
+  {
+    parent::__construct($json_error_response->{'description'}, $json_error_response->{'error_code'});
+  }
+}
+
+class APIClient {
+  public static $API_BASE_URI = 'http://api-incidentes.com/';
+  private $client;
+
+  public function __construct($api_base_uri = '')
+  {
+    if (empty($api_base_uri))
+      $api_base_uri = self::$API_BASE_URI;
+
+    $client = new \GuzzleHttp\Client(['base_uri' => $api_base_uri]);
+  }
+
+  public function get($endpoint)
+  {
+    return $this->executeRequest('GET', $endpoint);
+  }
+
+  public function post($endpoint, $args)
+  {
+    return $this->executeRequest('POST', $endpoint, $args);
+  }
+
+  private function executeRequest($method, $endpoint, $args = NULL)
+  {
+    try
+    {
+      $response = $this->client->request($method, $endpoint, ['form_params' => $args]);
+      if ($response->getStatusCode() == 400)
+        throw new \BadRequestException( \json_decode($response->getBody()->getContents()));
+
+      return $response;
+    }
+    catch (RequestException $e)
+    {
+      if ($e->hasResponse())
+        return ($e->getResponse());
+    }
+  }
+}
